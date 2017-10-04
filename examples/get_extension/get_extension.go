5936ff8e6d67f9e3efc51dd5b8e4a563a3e455b7
John Wang <johncwang@gmail.com>
2017-10-04 03:12:56 -0700

    patch to make oauth2+resty work

--- examples/get_extension/get_extension.go
+++ examples/get_extension/get_extension.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/grokify/gotilla/config"
+	"github.com/grokify/gotilla/fmt/fmtutil"
+	rcOAuth "github.com/grokify/oauth2util-go/services/ringcentral"
 	"github.com/grokify/ringcentral-sdk-go-scg"
 )
 
-const (
-	RINGCENTRAL_SERVER_URL_PRODUCTION = "https://platform.ringcentral.com"
-)
-
 func main() {
-	accessToken := os.Getenv(EnvAccessToken)
-	mediaId := os.Getenv(EnvMediaId)
+	err := config.LoadDotEnv()
+	if err != nil {
+		panic(err)
+	}
 
-	mediaApi := ringcentral.NewExtensionApi()
-	mediaApi.Configuration.SetAccessToken(accessToken)
+	client, err := rcOAuth.NewClientPassword(
+		rcOAuth.ApplicationCredentials{
+			ClientID:     os.Getenv("RC_CLIENT_ID"),
+			ClientSecret: os.Getenv("RC_CLIENT_SECRET"),
+			ServerURL:    os.Getenv("RC_SERVER_URL")},
+		rcOAuth.UserCredentials{
+			Username:  os.Getenv("RC_USER_USERNAME"),
+			Extension: os.Getenv("RC_USER_EXTENSION"),
+			Password:  os.Getenv("RC_USER_PASSWORD")})
+
+	extApi := ringcentral.NewExtensionApiWithBasePath(os.Getenv("RC_SERVER_URL"))
+	extApi.Configuration.Transport = client.Transport
+
+	extInfo, resp, err := extApi.RestapiV10AccountAccountIdExtensionExtensionIdGet("~", "~")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", resp.StatusCode)
+	fmtutil.PrintJSON(extInfo)
 
 	fmt.Println("DONE")
 }
