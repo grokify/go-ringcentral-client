889ca93f83b06c3892f36d6e605dbe7a8c91f8bf
grokify <johncwang@gmail.com>
2018-04-18 20:10:23 -0700

    fix `date-time` query parameter RFC 3339 format

--- codegen/swagger_codegen_command.sh
+++ codegen/swagger_codegen_command.sh
@@ -4,4 +4,5 @@ perl -p -i -e 's/, ([^\s]+) \*os\.File,/, localVarFile *os.File,/g' `grep -ril '
 grep '*os.File,' client/*.go
 grep '*interface{}' client/*.go
 perl -p -i -e 's/\*(interface\{\})/$1/g' `grep -ril '*interface{}' client/*.go`
+perl -p -i -e 's/(delimiter,\s+-1\),\s+"\[\]"\))/$1\n} else if t, ok := obj.(time.Time); ok {\nreturn t.Format(time.RFC3339)/g' client/api_client.go
 gofmt -s -w client/*.go
