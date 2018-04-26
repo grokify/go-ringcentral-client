5ff066cfc3c94eb00b48f2994062ae26ad0ee7ec
grokify <johncwang@gmail.com>
2018-04-26 01:47:32 -0700

    update glip_bot_webhooks_list

--- examples/glip/glip_bot_webhooks_list/main.go
+++ examples/glip/glip_bot_webhooks_list/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/caarlos0/env"
 	"github.com/grokify/gotilla/config"
 	"github.com/grokify/gotilla/fmt/fmtutil"
 	om "github.com/grokify/oauth2more"
 
+	rc "github.com/grokify/go-ringcentral/client"
 	ru "github.com/grokify/go-ringcentral/clientutil"
 )
 
@@ -18,6 +20,42 @@ type RingCentralConfig struct {
 	TokenJSON  string `env:"RINGCENTRAL_TOKEN_JSON"`
 	ServerURL  string `env:"RINGCENTRAL_SERVER_URL"`
 	WebhookURL string `env:"RINGCENTRAL_WEBHOOK_URL"`
+}
+
+func ReplaceSubscription(apiClient *rc.APIClient, subscription rc.SubscriptionResponse) error {
+	if subscription.Status == "Blacklisted" {
+		fmt.Printf("Blacklisted %v %v\n",
+			subscription.Id,
+			subscription.DeliveryMode.Address)
+
+		if strings.Index(subscription.Id, "amazonaws.com") >= 0 {
+			resp, err := apiClient.PushNotificationsApi.DeleteSubscription(
+				context.Background(), subscription.Id)
+			if err != nil {
+				return err
+			} else if resp.StatusCode >= 300 {
+				return fmt.Errorf("API Status %v", resp.StatusCode)
+			}
+
+			req := rc.CreateSubscriptionRequest{
+				EventFilters: []string{"/restapi/v1.0/glip/posts"},
+				DeliveryMode: &rc.NotificationDeliveryModeRequest{
+					TransportType: "WebHook",
+					Address:       subscription.DeliveryMode.Address},
+				ExpiresIn: int32(500000000)}
+
+			info, resp, err := apiClient.PushNotificationsApi.CreateSubscription(
+				context.Background(), req)
+
+			if err != nil {
+				return err
+			} else if resp.StatusCode >= 300 {
+				return fmt.Errorf("API Status %v", resp.StatusCode)
+			}
+			fmtutil.PrintJSON(info)
+		}
+	}
+	return nil
 }
 
 // This code takes a bot token and creates a permanent webhook.
@@ -56,10 +94,9 @@ func main() {
 	fmtutil.PrintJSON(info)
 
 	for _, subscription := range info.Records {
-		if subscription.Status == "Blacklisted" {
-			fmt.Printf("Blacklisted %v %v\n",
-				subscription.Id,
-				subscription.DeliveryMode.Address)
+		err := ReplaceSubscription(apiClient, subscription)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 
