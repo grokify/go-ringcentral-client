aac3556acda38329a9eb0716d06f6035faebb33d
grokify <johncwang@gmail.com>
2018-04-28 22:57:27 -0700

    update Glip support

---
+++ clientutil/glip.go
@@ -0,0 +1,59 @@
+package clientutil
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	rc "github.com/grokify/go-ringcentral/client"
+)
+
+type GlipApiUtil struct {
+	ApiClient *rc.APIClient
+}
+
+func (apiUtil *GlipApiUtil) GlipGroupMemberCount(groupId string) (int64, error) {
+	if apiUtil.ApiClient == nil {
+		return int64(-1), fmt.Errorf("GlipApiUtil is missing RingCentral ApiClient")
+	}
+	groupId = strings.ToLower(strings.TrimSpace(groupId))
+	grp, resp, err := apiUtil.ApiClient.GlipApi.LoadGroup(context.Background(), groupId)
+	if err != nil {
+		return int64(-1), err
+	} else if resp.StatusCode >= 300 {
+		return int64(-1), fmt.Errorf("Glip API Response Code [%v]", resp.StatusCode)
+	}
+	return int64(len(grp.Members)), nil
+}
+
+// DirectMessage means a group of 2 or a team of 2
+func (apiUtil *GlipApiUtil) AtMentionedOrGroupOfTwo(userId, groupId string, mentions []rc.GlipMentionsInfo) (bool, error) {
+	if IsAtMentioned(userId, mentions) {
+		return true, nil
+	}
+
+	count, err := apiUtil.GlipGroupMemberCount(groupId)
+	if err != nil {
+		return false, err
+	}
+	if count == int64(2) {
+		return true, nil
+	}
+	return false, nil
+}
+
+func IsAtMentioned(userId string, mentions []rc.GlipMentionsInfo) bool {
+	for _, mention := range mentions {
+		if userId == mention.Id {
+			return true
+		}
+	}
+	return false
+}
+
+func GlipCreatePostIsEmpty(post rc.GlipCreatePost) bool {
+	if len(strings.TrimSpace(post.Text)) == 0 && len(post.Attachments) == 0 {
+		return false
+	}
+	return true
+}
--- examples/glip/glip_bot_webhooks_list/main.go
+++ examples/glip/glip_bot_webhooks_list/main.go
@@ -5,16 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/caarlos0/env"
 	"github.com/grokify/gotilla/config"
 	"github.com/grokify/gotilla/fmt/fmtutil"
 	om "github.com/grokify/oauth2more"
+	"github.com/jessevdk/go-flags"
 
 	rc "github.com/grokify/go-ringcentral/client"
 	ru "github.com/grokify/go-ringcentral/clientutil"
 )
+
+type Options struct {
+	List     []bool `short:"l" long:"list" description:"List subscriptions"`
+	Delete   string `short:"d" long:"delete" description:"Delete subscriptions"`
+	Recreate string `short:"r" long:"recreate" description:"Recreate subscription"`
+}
 
 type RingCentralConfig struct {
 	TokenJSON  string `env:"RINGCENTRAL_TOKEN_JSON"`
@@ -28,39 +34,44 @@ func ReplaceSubscription(apiClient *rc.APIClient, subscription rc.SubscriptionRe
 			subscription.Id,
 			subscription.DeliveryMode.Address)
 
-		if strings.Index(subscription.Id, "amazonaws.com") >= 0 {
-			resp, err := apiClient.PushNotificationsApi.DeleteSubscription(
-				context.Background(), subscription.Id)
-			if err != nil {
-				return err
-			} else if resp.StatusCode >= 300 {
-				return fmt.Errorf("API Status %v", resp.StatusCode)
-			}
-
-			req := rc.CreateSubscriptionRequest{
-				EventFilters: []string{"/restapi/v1.0/glip/posts"},
-				DeliveryMode: &rc.NotificationDeliveryModeRequest{
-					TransportType: "WebHook",
-					Address:       subscription.DeliveryMode.Address},
-				ExpiresIn: int32(500000000)}
-
-			info, resp, err := apiClient.PushNotificationsApi.CreateSubscription(
-				context.Background(), req)
-
-			if err != nil {
-				return err
-			} else if resp.StatusCode >= 300 {
-				return fmt.Errorf("API Status %v", resp.StatusCode)
-			}
-			fmtutil.PrintJSON(info)
+		resp, err := apiClient.PushNotificationsApi.DeleteSubscription(
+			context.Background(), subscription.Id)
+		if err != nil {
+			return err
+		} else if resp.StatusCode >= 300 {
+			return fmt.Errorf("API Status %v", resp.StatusCode)
 		}
+
+		req := rc.CreateSubscriptionRequest{
+			EventFilters: []string{"/restapi/v1.0/glip/posts"},
+			DeliveryMode: &rc.NotificationDeliveryModeRequest{
+				TransportType: "WebHook",
+				Address:       subscription.DeliveryMode.Address},
+			ExpiresIn: int32(500000000)}
+
+		info, resp, err := apiClient.PushNotificationsApi.CreateSubscription(
+			context.Background(), req)
+
+		if err != nil {
+			return err
+		} else if resp.StatusCode >= 300 {
+			return fmt.Errorf("API Status %v", resp.StatusCode)
+		}
+		fmtutil.PrintJSON(info)
 	}
 	return nil
 }
 
 // This code takes a bot token and creates a permanent webhook.
 func main() {
-	err := config.LoadDotEnvSkipEmpty(os.Getenv("ENV_PATH"), "./.env")
+	opts := Options{}
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = config.LoadDotEnvSkipEmpty(os.Getenv("ENV_PATH"), "./.env")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -83,20 +94,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	info, resp, err := apiClient.PushNotificationsApi.GetSubscriptions(
-		context.Background())
+	if len(opts.List) > 0 || len(opts.Recreate) > 0 {
+		info, resp, err := apiClient.PushNotificationsApi.GetSubscriptions(
+			context.Background())
 
-	if err != nil {
-		log.Fatal(err)
-	} else if resp.StatusCode >= 300 {
-		log.Fatal(fmt.Errorf("API Status %v", resp.StatusCode))
-	}
-	fmtutil.PrintJSON(info)
-
-	for _, subscription := range info.Records {
-		err := ReplaceSubscription(apiClient, subscription)
 		if err != nil {
 			log.Fatal(err)
+		} else if resp.StatusCode >= 300 {
+			log.Fatal(fmt.Errorf("API Status %v", resp.StatusCode))
+		}
+		fmtutil.PrintJSON(info)
+
+		fmt.Println(opts.Recreate)
+
+		if len(opts.Recreate) > 0 {
+			for _, subscription := range info.Records {
+				if subscription.Id == opts.Recreate {
+					err := ReplaceSubscription(apiClient, subscription)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
 		}
 	}
 
