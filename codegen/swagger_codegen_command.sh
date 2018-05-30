a0ef115a42babb42f2105945611e702071e8c916
grokify <johncwang@gmail.com>
2018-05-30 01:08:57 -0700

    update swagger_codegen_command.sh

--- codegen/swagger_codegen_command.sh
+++ codegen/swagger_codegen_command.sh
@@ -1,3 +1,4 @@
+perl -p -i -e 's/\s+\[Beta\]\s*$/\n/g' swagger_spec.yaml
 java -jar swagger-codegen-cli.jar generate -c swagger_codegen_config.json -i swagger_spec.yaml -l go -o client
 grep '*os.File,' client/*.go
 perl -p -i -e 's/, ([^\s]+) \*os\.File,/, localVarFile *os.File,/g' `grep -ril ' *os.File,' client/*.go`
