91ca46c4933c238776cc57f06c007dc920b7974e
grokify <johncwang@gmail.com>
2018-11-02 04:45:19 -0700

    add upload_user_profile_image example & util

---
+++ clientutil/upload_user_profile_image.go
@@ -0,0 +1,34 @@
+package clientutil
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/grokify/gotilla/mime/multipartutil"
+	"github.com/grokify/gotilla/net/httputilmore"
+	"github.com/grokify/gotilla/net/urlutil"
+)
+
+func UploadUserProfileImage(httpClient *http.Client, serverUrl, accountId, extensionId, imgFilepath string) (*http.Response, error) {
+	builder := multipartutil.NewMultipartBuilder()
+	err := builder.WriteFilePathPlus("image", imgFilepath, false)
+	if err != nil {
+		return nil, err
+	}
+	err = builder.Close()
+	if err != nil {
+		return nil, err
+	}
+	//fmt.Println(builder.Buffer.String())
+
+	urlPath := fmt.Sprintf(`restapi/v1.0/account/%s/extension/%s/profile-image`, accountId, extensionId)
+	apiURL := urlutil.JoinAbsolute(serverUrl, urlPath)
+
+	req, err := http.NewRequest(http.MethodPost, apiURL, builder.Buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add(httputilmore.HeaderContentType, builder.ContentType())
+	return httpClient.Do(req)
+}
